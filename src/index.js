e07455c18861b596b7284576ea126d865f3c7007
Collins Muriuki <murerwacollins@gmail.com>
2020-05-04 17:57:22 +0300

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { GraphQLServer, PubSub } from "graphql-yoga";
+
+import { resolvers } from "./resolvers/index";
+
+const PORT = process.env.PORT || 4040;
+const pubsub = new PubSub();
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: {
+    pubsub,
+  },
+});
+
+server.start({ port: PORT }, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
