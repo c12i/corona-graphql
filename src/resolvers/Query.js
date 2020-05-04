e9f45e14567031d64d6ba423e4e73705390e9cd4
Collins Muriuki <murerwacollins@gmail.com>
2020-05-04 18:58:44 +0300

    Complete Queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -16,6 +16,31 @@ const Query = {
     }
     return data;
   },
+
+  getSingleCountryCases: async (root, { country }) => {
+    const { data } = await axios.get(
+      `https://corona.lmao.ninja/v2/countries/${country.toLowerCase()}`
+    );
+    return data;
+  },
+
+  allContinentStats: async (root, { query }) => {
+    const { data } = await axios.get("https://corona.lmao.ninja/v2/continents");
+    if (query) {
+      const filteredData = data.filter((p) =>
+        p.toLowerCase().includes(query.toLowercase())
+      );
+      return filteredData;
+    }
+    return data;
+  },
+
+  getSingleContinent: async (root, { continent }) => {
+      const { data } = await axios.get(
+        `https://corona.lmao.ninja/v2/continents/${continent.toLowerCase()}`
+      );
+      return data;
+  }
 };
 
 export default Query;
